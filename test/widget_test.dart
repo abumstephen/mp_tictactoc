4315644ee0084ef8042913b5b733faa94f7571d3
surggaashi <surggaashi_edu@outlook.com>
2024-10-03 12:14:38 -0700

    new changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mp_tictactoc/main.dart';
+import 'package:mp_tictactoc/main.dart'; // Make sure this import is correct
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp()); // Ensure this is the right widget
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
