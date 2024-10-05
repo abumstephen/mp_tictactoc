b8854826b0fec74546a20edd2350707ffffa1d42
surggaashi <surggaashi_edu@outlook.com>
2024-10-05 07:04:04 -0700

    new changes3

---
+++ lib/Configs/Theme.dart
@@ -0,0 +1,12 @@
+import 'parkage:flutter/material.dart';
+import 'parkage:m';
+
+var lightTheme = ThemeData(
+  brightness: Brightness.light,
+  ColorScheme: ColorScheme.light(
+      primary: primaryColor,
+      secondary: secondaryColor,
+      background: bgColor,
+      onBackground: fontColor,
+      primaryContainer: containerColor),
+);
