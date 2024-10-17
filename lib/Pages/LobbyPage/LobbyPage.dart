e89f2deeccfc1e2250e0b49f1d2e5c5f1aead66e
surggaashi <surggaashi_edu@outlook.com>
2024-10-17 12:15:26 -0700

    Lobby Page

---
+++ lib/Pages/LobbyPage/LobbyPage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mp_tictactoc/Components/primaryButton.dart';
+import 'package:mp_tictactoc/Configs/AssetsPath.dart';
+
+class Lobbypage extends StatelessWidget {
+  const Lobbypage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset(IconsPth.backIcon),
+                const SizedBox(width: 15),
+                Text(
+                  "Play With Private Room",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Text(
+              'Enter Private Code And Join With Your Friend',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                fillColor: Theme.of(context).colorScheme.primaryContainer,
+                filled: true,
+                hintText: "Enter Code Now",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Primarybutton(
+              buttonText: "Join Now",
+              onTap: () {},
+            ),
+            SizedBox(height: 80),
+            Text(
+              'Please Create A Private Room',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            Spacer(),
+            Primarybutton(
+              buttonText: "Create Room",
+              onTap: () {
+                Get.to(Lobbypage());
+              },
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
